1f6ac94ab1a75b69b346718ddb77d7c95fd104a8
Michelwas <mgwassing@gmail.com>
2022-05-23 00:20:52 +0200

    added: merge driver to resolve package lock merge conflict

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+/************************************************
+  * Express app
+  ***********************************************/
 
-// feedback in the console for starting app
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-// Middelware
-app.use('/public', express.static('public'));
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+ const express = require('express');
+ const app = express();
+ const port = 3000;
+ 
+ /************************************************
+   * Dummy data
+   ***********************************************/
+ const mensen = require('../../dummy-data/mensen');
+ 
+ /************************************************
+   * feedback in the console for starting app
+   ***********************************************/
+ 
+ app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`);
+ });
+ 
+ /************************************************
+   * Middleware
+   ***********************************************/
+ 
+ app.use('/public', express.static('public'));
+ app.set('view engine', 'ejs');
+ app.set('views', 'views');
+ 
+ /************************************************
+   * Routing
+   ***********************************************/
 
 app.get('/', (req, res) => {
-  res.render('pages/index');
+  res.render('pages/index', { data: mensen });
 });
 
 app.get('/match', (req, res) => {
-  res.render('pages/match');
+  res.render('pages/match', { data: mensen });
 });
 
 app.get('/admin', (req, res) => {
